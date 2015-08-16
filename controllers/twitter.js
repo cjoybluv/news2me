4a16275acb509c42880a0e2fd0ce3c064eb3e01f
msharif34 <msharif34@yahoo.com>
2015-08-16 15:40:18 -0700

    added twitter.js

---
+++ controllers/twitter.js
@@ -0,0 +1,50 @@
+var db = require('../models');
+var express = require('express');
+var router = express.Router();
+var Twitter = require('twitter');
+ 
+
+ router.get('/', function(req,res){
+ 	// res.send('hello');
+	  var client = new Twitter({
+	  consumer_key: 'kx91S8r8LCMFDcFuEpUzO9HFy',
+	  consumer_secret: 'EFdGpEojTvmzmVja6frfDmtIJngUMXa1wg3nKm2BO0LxQ6gIYe',
+	  access_token_key: '18682848-BrD9Z1S7ZLj7NnwkwIfE9PVsMA7TjaOJ6lGs8LxKY',
+	  access_token_secret: 'VTvJ515RX5TR9sLRQH7MAcOcJl2Wdez48axmopZ9OoDfh'
+	});
+
+client.get('search/tweets', {'q':'#trump  since:2015-08-01', 'count': 10, 'result\_type':'popular'}, function(error, tweets, response){
+  if(error) throw error;
+  //  console.log(tweets);  // The favorites. 
+  // console.log(response);  // Raw response object. 
+
+	res.render('twitter/index', {tweets: tweets})
+	// res.render('twitter', {tweets: tweets})
+});
+//check limit on API
+// client.get('application/rate_limit_status', function(error, limit, response){
+//   if(error) throw error;
+//   // console.log(response);  // Raw response object. 
+// 	res.send(limit)
+// 	});
+ // db.channel.find({
+  // var defaultChannel = 'presidentElect2016';
+ //    where:{
+ //      name: defaultChannel
+ //    }
+ //  }).then(function(channel){
+ //    var result = channel.get().search_terms.split('///').map(function(term){
+ //      return '@'+term.replace(/ /gi, '').toLowerCase()});
+
+
+ //    console.log('@array',result);
+ //    // // do something with this result HERE!!! like...
+ //    res.render('twitter/index', {
+ //      channel_name: channel.get().name,
+ //      search_terms: result
+ //    });
+ //  });
+
+});
+
+module.exports = router;
