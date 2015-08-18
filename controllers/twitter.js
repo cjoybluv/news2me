4408b7e9e3292742da0d6096d2b8c5bc28d09d3f
msharif34 <msharif34@yahoo.com>
2015-08-17 17:31:50 -0700

    added sentiment score to data

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -3,48 +3,89 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 var async = require('async');
-
+var sentiment = require('sentiment');
 
 router.get('/', function(req,res){
-        console.log(process.env);
-   // res.send('hello');
-     var client = new Twitter({
-         consumer_key: process.env.TWITTR_CONSUMER_KEY,
-         consumer_secret: process.env.TWITTR_CONSUMER_SECRET,
-         access_token_key: process.env.TWITTR_ACCESS_TOKEN_KEY,
-         access_token_secret: process.env.TWITTR_ACCESS_TOKEN_SECRET
-                  // consumer_key: 'V52R2iRmZlA3RowHVnKYRoqHE',
-                  // consumer_secret: 'ufZZyF9SnLBZ1mNRLHIevQaG13kC8mG4GlN8BUQ7RfvNlrj0U1',
-                  // access_token_key: '18682848-BrD9Z1S7ZLj7NnwkwIfE9PVsMA7TjaOJ6lGs8LxKY',
-                  // access_token_secret: 'VTvJ515RX5TR9sLRQH7MAcOcJl2Wdez48axmopZ9OoDfh'
+    // res.send('hello');
+      var client = new Twitter({
+        consumer_key: process.env.TWITTR_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTR_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTR_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTR_ACCESS_TOKEN_SECRET
+        });
 
-       });
-       var candidates = ['Hillary Clinton', 'Donald Trump', 'Bernie Sanders'];
+  var defaultChannel = 'presidentElect2016';
+  var words = {
+	  'dangerous': -3,
+	  'abortion': -2,
+	  'ultimate': 5,
+	  'joining': 3,
+	  'forces': 4,
+	  'crazy': -1,
+	  'dissatisfaction': 3,
+	  'anncoulter': -2
+	};
+  db.channel.find({
+     where:{
+       name: defaultChannel
+     }
+   }).then(function(channel){
+     var result = channel.get().search_terms.split('///').map(function(term){
+       // return '@'+term.replace(/ /gi, '').toLowerCase()});
+                return term});
 
-       async.map(candidates, function(candidate, callback) {
-         console.log("Searching for tweets on  : " + candidate);
-        client.get('search/tweets', {'q': candidate + ' since:2015-08-01', 'count': 30, 'result\_type':'popular'},
-           function(error, tweets, response){
-             if(error) throw error;
-              // console.log(tweets);  // The favorites.
-              callback(null,{
-                  term:candidate,
-                  tweets:tweets
-              });
+     console.log('@array',result);
+     // // do something with this result HERE!!! like...
 
-           });
-       }, function(err,result) {
-           // console.log(result);
-            res.render('twitter/index', {result: result})
-           result.forEach(function(person){
-               console.log(person.term);
-               console.log(person.tweets.statuses.length);
-               console.log(person.tweets.statuses.text);
-               console.log('-----')
-           })
-             console.log("DONE WITH EVERYTHING");
-       });
+      var candidates = result;
+
+      async.map(candidates, function(candidate, callback) {
+        console.log("Searching for tweets on  : " + candidate);
+       client.get('search/tweets', {'q': candidate + ' since:2015-08-01', 'count': 30, 'result\_type':'popular'},
+          function(error, tweets, response){
+            if(error) throw error;
+             // console.log(tweets);  // The favorites.
+
+             tweets.statuses = tweets.statuses.map(function(status){
+             	status.sentiment = sentiment(status.text,words);
+             	return status
+             });
+
+             tweets.status = tweets.statuses.sort(function(a,b){
+             	return b.retweet_count - a.retweet_count;
+             });
+
+             callback(null,{
+                 term:candidate,
+                 tweets:tweets
+             });
+
+          });
+      }, function(err,result) {
+          // console.log(result);
+           // res.send({result: result});
+         // res.send(result);
+          res.render('twitter/index', {result: result});
+          // result.forEach(function(person){
+          //     console.log(person.term);
+          //     console.log(person.tweets.statuses.length);
+          //     console.log(person.tweets.statuses.text);
+          //     console.log('-----')
+          // })
+            // console.log("DONE WITH EVERYTHING");
+      });
+
+   });
+
 
 });
+
+
+//check limit on API
+// client.get('application/rate_limit_status', function(error, limit, response){
+//   if(error) throw error;
+//   // console.log(response);  // Raw response object.
+//     res.send(limit)
+//     });
 
 module.exports = router;
